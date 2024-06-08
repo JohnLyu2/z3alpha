87965e20873bc154fcce97667a76dac9a17d156f
John Lu <z52lu@uwaterloo.ca>
2024-06-08 10:18:28 -0400

    check result script

---
+++ scripts/result_checker.py
@@ -0,0 +1,50 @@
+import csv
+
+TARGET_CSV = "/Users/zhengyanglu/Desktop/z3alpha/smtcomp24/results/QF_AX/z3.csv"
+
+COMPARE_CSV_LST = [
+    "/Users/zhengyanglu/Desktop/z3alpha/smtcomp24/results/QF_AX/cvc5.csv"
+]
+
+# return the substring after "non-incremental/"
+def trim_instance_path(instance_path):
+    # assert "non-incremental/" in instance_path
+    assert "non-incremental/" in instance_path
+    return instance_path.split("non-incremental/", 1)[1]
+
+def read_csv(csv_path):
+    # the csv has a header; read the csv into a dictionary
+    csv_dict = {}
+    with open(csv_path, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)
+        for row in reader:
+            instance_path = row[1]
+            trimmed_instance_path = trim_instance_path(instance_path)
+            solved = True if row[2] == "True" else False
+            sat = None
+            if solved:
+                sat = True if row[4] == "sat" else False
+            csv_dict[trimmed_instance_path] = (solved, sat)
+    return csv_dict
+                
+def compare_two_csv(target_csv, compare_csv):
+    target_dict = read_csv(target_csv)
+    compare_dict = read_csv(compare_csv)
+    mismatch = False
+    for instance_path, (solved, sat) in target_dict.items():
+        if solved and (instance_path in compare_dict):
+            compare_solved, compare_sat = compare_dict[instance_path]
+            if compare_solved and (sat != compare_sat):
+                print(f"{instance_path} result mismatch: {sat} in {target_csv} vs {compare_sat} in {compare_csv}")
+                mismatch = True
+    if not mismatch:
+        print(f"Match: {target_csv} and {compare_csv}")
+
+def main():
+    for compare_csv in COMPARE_CSV_LST:
+        compare_two_csv(TARGET_CSV, compare_csv)
+
+if __name__ == "__main__":
+    main()
+    
