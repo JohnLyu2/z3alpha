883bcf550ce4f6de81f8f764986f6cec8703aa03
John Lu <z52lu@uwaterloo.ca>
2024-06-13 15:52:58 -0400

    update to 900 strategies

--- smtcomp24/z3alpha_submission/z3alpha.py
+++ smtcomp24/z3alpha_submission/z3alpha.py
@@ -5,22 +5,23 @@ import os
 import subprocess
 
 logic_strategy = {
-    "UFDT": "ufdt300.txt",
-    "QF_BV": "qfbv300.txt",
-    "QF_DT": "qfdt300.txt",
-    "QF_UFDT": "qfufdt300.txt",
-    "QF_AUFBV": "qfaufbv300.txt",
-    "QF_UFBV": "qfufbv300.txt",
-    "QF_AX": "qfax300.txt",
-    "QF_UF": "qfuf300.txt",
-    "QF_IDL": "qfidl300.txt",
-    "QF_LIA": "qflia300.txt",
-    "QF_LRA": "qflra300.txt",
-    "QF_RDL": "qfrdl300.txt",
-    "QF_NIA": "qfnia300.txt",
-    "QF_NRA": "qfnra300.txt",
-    "QF_S": "qfs300.txt",
-    "QF_SLIA": "qfslia300.txt"
+    "BV": "bv900.txt",
+    "UFDT": "ufdt900.txt",
+    "QF_BV": "qfbv900.txt",
+    "QF_DT": "qfdt900.txt",
+    "QF_UFDT": "qfufdt900.txt",
+    "QF_AUFBV": "qfaufbv900.txt",
+    "QF_UFBV": "qfufbv900.txt",
+    "QF_AX": "qfax900.txt",
+    "QF_UF": "qfuf900.txt",
+    "QF_IDL": "qfidl900.txt",
+    "QF_LIA": "qflia900.txt",
+    "QF_LRA": "qflra900.txt",
+    "QF_RDL": "qfrdl900.txt",
+    "QF_NIA": "qfnia900.txt",
+    "QF_NRA": "qfnra900.txt",
+    "QF_S": "qfs900.txt",
+    "QF_SLIA": "qfslia900.txt"
 }
 
 def read_smtlib_logic(smt2_str):
