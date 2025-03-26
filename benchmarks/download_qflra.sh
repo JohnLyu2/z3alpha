622a58e9ccbcfa768eff2ef8fe10cc320230d652
John Lu <z52lu@uwaterloo.ca>
2025-03-26 14:59:00 +0100

    change paths in benchmark creation scripts to relative paths

--- benchmarks/download_qflra.sh
+++ benchmarks/download_qflra.sh
@@ -1,2 +1,2 @@
 git clone https://clc-gitlab.cs.uiowa.edu:2443/SMT-LIB-benchmarks/QF_LRA.git
-python ../scripts/dataset_create.py --split_size "250 500" --benchmark_dir  ./QF_LRA --dataset_dir /home/z52lu/alphasmt/benchmarks/qflra_exp
+python ../scripts/dataset_create.py --split_size "250 500" --benchmark_dir  ./QF_LRA --dataset_dir ./qflra_exp
--- benchmarks/download_qfs.sh
+++ benchmarks/download_qfs.sh
@@ -1,2 +1,2 @@
 git clone https://clc-gitlab.cs.uiowa.edu:2443/SMT-LIB-benchmarks/QF_S.git
-python ../scripts/dataset_create.py --split_size "250 500" --benchmark_dir  ./QF_S --dataset_dir /home/z52lu/alphasmt/benchmarks/qfs_exp
+python ../scripts/dataset_create.py --split_size "250 500" --benchmark_dir  ./QF_S --dataset_dir ./qfs_exp
